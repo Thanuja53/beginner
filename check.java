08e48f315b0b3310e7d604c15659fd9f9cdd8b1f
THANUJA J <vrash53@gmail.com>
2017-06-05 09:38:26 -0700

    Update and rename README.md to check.java

---
+++ check.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class check
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n!=0)
+		{
+		 if(n>0)
+		 System.out.println("positive");
+         else	 
+         System.out.println("negative");  
+        }
+		else
+		System.out.println("zero");	
+    }
+}
