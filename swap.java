8810220c1576276ee45b7b828b8ede9cbf7e5a98
THANUJA J <vrash53@gmail.com>
2017-06-06 10:21:26 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class swap
+{
+  public static void main(String args[])
+  {
+    char[] ch=args[0].toCharArray();
+	if(args[0].length()%2==0)
+	{
+    for(int i=0;i<args[0].length();i=i+2)
+    {		
+	  System.out.print(ch[i+1]);
+      System.out.print(ch[i]);
+    }
+	}
+	else
+	{
+    for(int i=0;i<args[0].length()-1;i=i+2)
+    {		
+	  System.out.print(ch[i+1]);
+      System.out.print(ch[i]);
+    }
+	System.out.print(ch[args[0].length()-1]);
+	}
+		
+  }
+}
