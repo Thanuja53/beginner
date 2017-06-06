a7aef70083a6545b71e661f2f4508dadbc3a2888
THANUJA J <vrash53@gmail.com>
2017-06-06 10:23:01 +0530

    Update swap.java

--- swap.java
+++ swap.java
@@ -3,23 +3,23 @@ class swap
 {
   public static void main(String args[])
   {
-    char[] ch=args[0].toCharArray();
+    char c[]=args[0].toCharArray();
 	if(args[0].length()%2==0)
 	{
     for(int i=0;i<args[0].length();i=i+2)
     {		
-	  System.out.print(ch[i+1]);
-      System.out.print(ch[i]);
+	  System.out.print(c[i+1]);
+      System.out.print(c[i]);
     }
 	}
 	else
 	{
     for(int i=0;i<args[0].length()-1;i=i+2)
     {		
-	  System.out.print(ch[i+1]);
-      System.out.print(ch[i]);
+	  System.out.print(c[i+1]);
+      System.out.print(c[i]);
     }
-	System.out.print(ch[args[0].length()-1]);
+	System.out.print(c[args[0].length()-1]);
 	}
 		
   }
