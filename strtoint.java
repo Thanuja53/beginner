c5f653c7f772c9ca7122ca40f91006510be7def3
THANUJA J <vrash53@gmail.com>
2017-06-06 18:50:10 +0530

    Create strtoint.java

---
+++ strtoint.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class strtoint
+{
+public static void main(String s[])
+{
+ Scanner sc=new Scanner(System.in);
+ String s1=sc.nextLine();
+ int n=0;
+ n=Integer.valueOf(s1);
+ System.out.println(n);
+ }
+}
