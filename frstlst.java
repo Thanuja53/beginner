f1e50d69635178205a4b61a63488150bf4893107
THANUJA J <vrash53@gmail.com>
2017-06-08 11:06:58 +0530

    Create frstlst.java

--- frstlst.java
+++ frstlst.java
@@ -3,7 +3,18 @@ class frstlst
 {
 public static void main(String s[])
 {
-  int n=Integer.parseInt(args[0]);
-  System.out.println(n[1]);
+  Scanner sc=new Scanner(System.in);
+  String n=sc.nextLine();
+  int l=n.length();
+  String s1[]=n.split("");
+  int[] a=new int[l];
+  int sum=0;
+  for(int i=0;i<l;i++)
+  {
+      a[i]=Integer.valueOf(s1[i]);
+  }
+  sum=a[0]+a[l-1];
+  System.out.println(sum);
 }
-}  
+}
+
