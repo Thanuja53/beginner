7d35497b7d968002aadeb4f343489ee2aeadd161
THANUJA J <vrash53@gmail.com>
2017-06-08 10:35:30 +0530

    Create oddsum.java

--- oddsum.java
+++ oddsum.java
@@ -4,12 +4,16 @@ class oddsum
 public static void main(String s[])
 {
 Scanner sc=new Scanner(System.in);
-String s=sc.nextLine();
-String ss[s.length()]=s.split("");
-int[] a=new int[s.length()];
-for(int i=0;i<a.length;i++)
+int n=sc.nextInt();
+int[] a=new int[n];
+int sum=0;
+for(int i=0;i<n;i++)
 {
-a[i]=Integer.valueOf(ss[i]);
+a[i]=sc.nextInt();
 if(a[i]%2!=0)
-{
+sum=sum+a[i];
+}  
+System.out.println(sum);
+}
+}  
 
