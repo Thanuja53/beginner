b58399526db00dd9f9947a727f77201a5814eed8
THANUJA J <vrash53@gmail.com>
2017-06-08 11:48:52 +0530

    Create isbn.java

--- isbn.java
+++ isbn.java
@@ -4,21 +4,27 @@ class isbn
 public static void main(String args[])
 {
     Scanner sc=new Scanner(System.in);
-    String a=sc.nextLine();
-    String b[]=a.split("");
-    int l=a.length();
-    int g[]=new int[l];
-    int sum=0;
-    for(int i=0;i<l;i++)
-      g[i]=Integer.valueOf(b[i]);
-    for(int j=0;j<l;j++)
-    {
-      sum=sum+g[i]*l;
-      --l;
-    }
+       String n=sc.nextLine();
+       int l=n.length();
+       int n2,i=0,sum=0;
+       int[] a=new int[l];
+       int n1=Integer.parseInt(n);
+       while(n1!=0)
+       {
+           n2=n1%10;
+           a[i]=n2;
+           i++;
+           n1=n1/10;
+       }
+       int j=0;
+       for(i=1;i<l;i++)
+       {
+           sum=sum+a[j]*i;
+           j++;
+       }
        if(sum%11==0)
-           System.out.println("isbn number");
+           System.out.println("it is ISBN");
        else
-           System.out.println("not a isbn number");
- }
+           System.out.println("not a ISBN NUMBER");
+}
 }
